5ae576d36da231b8d7a965791e66d04d41379bd8
Dave Lambley <dave@lambley.me.uk>
2019-10-27 20:42:47 +0000

    Give license.

--- lib/RTF/Encode.pm
+++ lib/RTF/Encode.pm
@@ -92,6 +92,11 @@ L<http://www.biblioscape.com/rtf15_spec.htm>
 
 Dave Lambley <dlambley@cpan.org>
 
+=head1 LICENSE
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
 =cut
 
 1;
